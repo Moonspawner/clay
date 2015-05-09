54359cd3ccb0ffb88f4475b9db01286be2902bf2
Jonathan <dr4yyee@gmail.com>
2015-05-09 20:02:30 +0200

    simple Lexing

---
+++ src/clay/Lexer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clay
+{
+    static class Lexer
+    {
+        public static IEnumerable<string> Lex(StreamReader code)
+        {
+            var lastToken = "";
+            while (!code.EndOfStream) {
+                if (code.Peek() == ' ') {
+                    if (lastToken != "") {
+                        yield return lastToken;
+                        lastToken = "";
+                    }
+                    code.Read();
+                } else {
+                    lastToken += (char)code.Read();
+                }
+            }
+            if (lastToken != "") { yield return lastToken; }
+        }
+    }
+}
---
+++ src/clay/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clay
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            var stream = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes("test1 test2  test3   ")));
+
+            foreach (var token in Lexer.Lex(stream)) {
+                Console.WriteLine("-{0}-", token);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
