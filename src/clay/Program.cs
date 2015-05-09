3dd7f5cfcf43488bf761562b013a68560a2261ae
Jonathan <dr4yyee@gmail.com>
2015-05-09 22:43:38 +0200

    added (immutable) variables

--- src/clay/Program.cs
+++ src/clay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,18 +11,27 @@ namespace clay
     class Program
     {
         static void Main(string[] args) {
-            var stream = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(@"tell 'I could only take your hand'
-tell 'and lead you to the crossing road where we first met,'
-tell 'we’d take a skip together like the good old days'
-tell 'and laugh beneath a brilliantly blue sky.'")));
+            var stream = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(@"
+set himmel sky
+
+tell 'I could only take your hand' $newline ''
+tell 'and lead you to the crossing road where we first met,' $newline ''
+tell 'we’d take a skip together like the good old days' $newline ''
+tell 'and laugh beneath a brilliantly blue ' $himmel $newline ''
+")));
+
+            var vars = new Dictionary<string, object> {{"$newline", "\n\r"}};
 
             foreach (var lineArray in Lexer.Lex(stream).Select(line => line.ToArray())) {
-                switch(lineArray[0])
-                {
+                switch(lineArray[0]) {
                     case "tell":
                         foreach (var argument in Enumerable.Range(1, lineArray.Count() - 1).Select(i => lineArray[i])) {
-                            Console.Write(argument.Replace("\r", "")); //ignore \r for now since it will make lines overlap when there is a \n\r in the original file >_>
+                            Console.Write(argument[0] == '$' ? vars[argument] : argument.Replace("\r", "")); //ignore \r for now since it will make lines overlap when there is a \n\r in the original file >_>
                         }
+                        break;
+
+                    case "set":
+                        vars.Add('$' + lineArray[1], lineArray[2]);
                         break;
                 }
             }
