d553e3f431d4c0a89a3da0e9a96cc5cd0fb16069
Jonathan <dr4yyee@gmail.com>
2015-05-10 03:01:30 +0200

    added enough functions to allow implementation of fibonacci sequence generator e.g. jumps and basic arithmetic, bigint

--- src/clay/Program.cs
+++ src/clay/Program.cs
@@ -1,42 +1,103 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace clay
 {
-    class Program
-    {
+    class Program {
+        private static Dictionary<string, string> _vars;
+        private static Dictionary<string, int> _jumps; 
+
         static void Main(string[] args) {
             var stream = new StreamReader(new MemoryStream(Encoding.UTF8.GetBytes(@"
-set himmel sky
+tell 'fibonacci sequence' $newline
+set $old 1
+set $current 1
+set $new $current
+tell $old $newline
+tell $current $newline
+marker repeat
+set $new $current
++ $new $old
+tell $new $newline
+set $old $current
+set $current $new
+sleep 350
+jump repeat
+".Replace("\r", ""))));
 
-tell 'I could only take your hand' $newline ''
-tell 'and lead you to the crossing road where we first met,' $newline ''
-tell 'we’d take a skip together like the good old days' $newline ''
-tell 'and laugh beneath a brilliantly blue ' $himmel $newline ''
-")));
+            _vars = new Dictionary<string, string> {{"$newline", "\n\r"}};
+            _jumps = new Dictionary<string, int>();
 
-            var vars = new Dictionary<string, object> {{"$newline", "\n\r"}};
+            var lines = Lexer.Lex(stream).ToArray();
+            for (var index = 0; index < lines.Length; index++) {
+                var tokens = lines[index].ToArray();
 
-            foreach (var lineArray in Lexer.Lex(stream).Select(line => line.ToArray())) {
-                switch(lineArray[0]) {
-                    case "tell":
-                        foreach (var argument in Enumerable.Range(1, lineArray.Count() - 1).Select(i => lineArray[i])) {
-                            Console.Write(argument[0] == '$' ? vars[argument] : argument.Replace("\r", "")); //ignore \r for now since it will make lines overlap when there is a \n\r in the original file >_>
-                        }
-                        break;
+                if (tokens.Any()) {
+                    switch (tokens[0]) {
+                        case "tell":
+                            foreach (
+                                var argument in Enumerable.Range(1, tokens.Count() - 1).Select(i => tokens[i])) {
+                                Console.Write(Resolve(argument));
+                            }
+                            break;
 
-                    case "set":
-                        vars.Add('$' + lineArray[1], lineArray[2]);
-                        break;
+                        case "set":
+                            _vars[tokens[1]] = Resolve(tokens[2]);
+                            break;
+
+                        case "read":
+                            _vars[tokens[1]] = Console.ReadLine();
+                            break;
+
+                        case "+":
+                            _vars[tokens[1]] =
+                                (BigInteger.Parse(Resolve(tokens[1])) + BigInteger.Parse(Resolve(tokens[2]))).ToString();
+                            break;
+
+                        case "-":
+                            _vars[tokens[1]] =
+                                (BigInteger.Parse(Resolve(tokens[1])) - BigInteger.Parse(Resolve(tokens[2]))).ToString();
+                            break;
+
+                        case "*":
+                            _vars[tokens[1]] =
+                                (BigInteger.Parse(Resolve(tokens[1]))* BigInteger.Parse(Resolve(tokens[2]))).ToString();
+                            break;
+
+                        case "/":
+                            _vars[tokens[1]] =
+                                (BigInteger.Parse(Resolve(tokens[1]))/ BigInteger.Parse(Resolve(tokens[2]))).ToString();
+                            break;
+
+                        case "abs":
+                            _vars[tokens[1]] = Resolve(tokens[1]);
+                            break;
+
+                        case "marker":
+                            _jumps[Resolve(tokens[1])] = index;
+                            break;
+
+                        case "jump":
+                            index = _jumps[Resolve(tokens[1])];
+                            break;
+
+                        case "sleep":
+                            Thread.Sleep(int.Parse(tokens[1]));
+                            break;
+                    }
                 }
             }
 
             Console.ReadLine();
+        }
+
+        private static string Resolve(string identifier) {
+            return identifier == "" ? "" : (identifier[0] == '$' ? _vars[identifier] : identifier);
         }
     }
 }
